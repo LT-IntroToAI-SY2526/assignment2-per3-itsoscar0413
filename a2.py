64a807180eab668fee8a14d4525ba3b971d84e95
Oscar David huerta <odhuerta@cps.edu>
2025-09-19 10:18:22 -0500

    did 2 more problems

--- a2.py
+++ a2.py
@@ -28,6 +28,10 @@ def match(pattern: List[str], source: List[str]) -> List[str]:
 
         # 1) if we reached the end of the pattern but not source
 
+        if pind == len(pattern) and sind < len(source):
+            print("End of pattern, but not source")
+            return None
+
         # 2) if the current thing in the pattern is a %
         # WARNING: this condition contains the bulk of the code for the assignment
         # If you get stuck on this one, we encourage you to attempt the other conditions
@@ -35,12 +39,16 @@ def match(pattern: List[str], source: List[str]) -> List[str]:
 
         # 3) if we reached the end of the source but not the pattern
 
+        elif sind == len(source) and pind < len(pattern):
+            print("End of source, but not pattern")
+            return None
+
         # 4) if the current thing in the pattern is an _
 
         # 5) if the current thing in the pattern is the same as the current thing in the
         # source
 
-        if pattern[pind] == source[sind]:
+        elif pattern[pind] == source[sind]:
             print(pattern[pind], source[sind])
             pind += 1
             sind += 1
@@ -50,6 +58,7 @@ def match(pattern: List[str], source: List[str]) -> List[str]:
         # source
 
         else:
+            print(pattern[pind] + " " + source[sind])
             print("End of Assert, return none")
             return None
 
